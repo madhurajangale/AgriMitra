adde8b4e308ff7b5a32f1e3f87a61d5e61f252ed
ShravaniAnilPatil <shravanipatil1427@gmail.com>
2025-10-01 15:15:03 +0530

    tailwind configured

---
+++ mitra/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css';
+
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-red-500">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+
