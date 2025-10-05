296b33044e718119563fbca151da9e75a6e199a5
IT <madhurajangle2004@gmail.com>
2025-10-05 18:17:58 +0530

    created home page and login page

--- mitra/src/App.jsx
+++ mitra/src/App.jsx
@@ -3,14 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css';
+import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Auth/login';
+// import Signup from './Auth/Signup';
+
 
 
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-red-500">
-      <h1 className="text-white text-4xl font-bold">
-        Tailwind is working!
-      </h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} /> */}
+        <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
